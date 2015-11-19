7881be60a81dd926a02b4c82941858ccb437aea6
Jim McAndrew <jim@loc8.us>
2015-11-19 16:08:24 -0700

    Add a map and the export script

--- scripts/export_geojson.sh
+++ scripts/export_geojson.sh
@@ -1,1 +1,9 @@
-# checkins_in_denver_neighborhoods_distinct.sql
+host=localhost
+user=postgres
+password=postgres
+dbname=postgis_day
+connection_string="host=$host user=$user dbname=$dbname password=$password"
+sql=`cat ../sql/checkins_in_denver_neighborhoods_distinct.sql`
+sql=${sql::-1}
+
+ogr2ogr -f "GeoJSON" ../data/my_checkins.geojson PG:"$connection_string" -sql "$sql"
